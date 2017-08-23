23ed77eed1ba5ad3a0fb759c6f8e1ea9f19ad8e6
siranui <siranui87@gmail.com>
2017-08-23 14:43:28 +0900

    fmt: BatchNorm.scala

--- src/main/scala/library/BatchNorm.scala
+++ src/main/scala/library/BatchNorm.scala
@@ -2,67 +2,78 @@ package pll
 import breeze.linalg._
 
 class BatchNorm(ch:Int = 1, dim: Int, update_method: String = "SGD", lr: Double = 0.01) extends Layer {
+  // 学習対象
+  var gamma = DenseVector.ones[Double](ch*dim)
+  var beta  = DenseVector.zeros[Double](ch*dim)
+
+  // backward()で使うため、forward()の外で定義している
+  var xc  = Array[DenseVector[Double]]()
+  var std = DenseVector[Double]()
+  var xn  = Array[DenseVector[Double]]()
+
+  // 更新量を保持する変数
+  var dgamma = DenseVector.zeros[Double](ch*dim)
+  var dbeta  = DenseVector.zeros[Double](ch*dim)
+
   var opt = Opt.create(update_method,lr)
+  opt.register(Array(gamma,beta))
 
   val eps = 1e-8
 
-  var gamma = DenseVector.ones[Double](ch*dim)
-  var beta = DenseVector.zeros[Double](ch*dim)
+  override def forwards(B: Array[DenseVector[Double]]): Array[DenseVector[Double]] = {
+    val batch_size = B.length.toDouble
 
-  var batch_size = 0d
-  var xc = Array[DenseVector[Double]]()
-  var sig2 = DenseVector[Double]()
-  var std = DenseVector[Double]()
-  var xn = Array[DenseVector[Double]]()
-
-  var dgamma = DenseVector.zeros[Double](ch*dim)
-  var dbeta = DenseVector.zeros[Double](ch*dim)
-
-  opt.register(Array(gamma,beta))
-
-  override def forwards(B: Array[DenseVector[Double]]) = {
-    batch_size = B.size
-    val mu = B.reduce(_+_) /:/ batch_size
+    val mu = B.reduce(_ + _) /:/ batch_size
     xc = B.map(_ - mu)
-    sig2 = xc.map(i => i *:* i).reduce(_+_) /:/ batch_size
+
+    val sig2 = xc.map(i => i *:* i).reduce(_ + _) /:/ batch_size
     std = (sig2 +:+ eps).map(math.sqrt)
+
     xn = xc.map(_ /:/ std)
+
     xn.map(x => gamma *:* x + beta)
   }
 
-  override def backwards(d: Array[DenseVector[Double]]) = {
-    dbeta += d.reduce(_+_)
+  override def backwards(d: Array[DenseVector[Double]]): Array[DenseVector[Double]] = {
+    dbeta  += d.reduce(_+_)
     dgamma += (xn zip d).map(i => i._1 *:* i._2).reduce(_+_)
+
+    val batch_size = d.length.toDouble
+
     val dxn = d.map(_ *:* gamma)
-    var dxc = dxn.map(_ /:/ std)
-    val dstd = - (dxn zip xc).map(i => (i._1 *:* i._2) /:/ (std *:* std)).reduce(_+_)
+
+    val dstd  = - (dxn zip xc).map(i => (i._1 *:* i._2) /:/ (std *:* std)).reduce(_+_)
     val dsig2 = 0.5 *:* dstd /:/ std
-    dxc = (dxc zip xc.map(_ *:* 2d *:* dsig2 /:/ batch_size)).map(i => i._1 + i._2)
+
+    val dxc_1 = dxn.map(_ /:/ std)
+    val dxc_2 = xc.map(_ *:* 2d *:* dsig2 /:/ batch_size)
+    val dxc   = (dxc_1 zip dxc_2).map(i => i._1 + i._2)
+
     val dmu = dxc.reduce(_+_)
-    val dx = dxc.map(i => (i - dmu) / batch_size)
-    dx
+
+    dxc.map(i => (i - dmu) / batch_size)
   }
 
   def update() {
-    val grads = opt.update(Array(gamma,beta),Array(dgamma,dbeta))
+    val grads = opt.update(Array(gamma, beta), Array(dgamma, dbeta))
 
     gamma = gamma - grads(0)
-    beta = beta - grads(1)
+    beta  = beta  - grads(1)
   }
 
-  def reset() = {
+  def reset() {
     dgamma = DenseVector.zeros[Double](ch*dim)
-    dbeta = DenseVector.zeros[Double](ch*dim)
-    batch_size = 0d
-    xc = Array[DenseVector[Double]]()
-    sig2 = DenseVector[Double]()
-    std = DenseVector[Double]()
-    xn = Array[DenseVector[Double]]()
+    dbeta  = DenseVector.zeros[Double](ch*dim)
+    xc     = Array[DenseVector[Double]]()
+    std    = DenseVector[Double]()
+    xn     = Array[DenseVector[Double]]()
   }
 
   def forward(x: DenseVector[Double]): DenseVector[Double] = x
   def backward(d: DenseVector[Double]): DenseVector[Double] = d
   def save(filename: String) {}
   def load(filename: String) {}
-  def load(data: List[String]) = {data}
+  def load(data: List[String]): List[String] = {
+    data
+  }
 }
