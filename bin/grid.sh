b14b885bc28a52a6685f3924e71ade5c5594a8af
siranui <siranui87@gmail.com>
2017-10-18 00:09:03 +0900

    fix

--- bin/grid.sh
+++ bin/grid.sh
@@ -1,33 +1,32 @@
 #!/bin/bash
 
 file_path=$1
-gw=32
-gh=32
-echo $file_path
-echo $gw
-echo $gh
+size=$(identify -format "%wx%h" $file_path)
+gw=32 # grid width
+gh=32 # grid height
+
+echo "target:      $file_path"
+echo "image size:  $size"
+echo "grid width:  $gw"
+echo "grid height: $gh"
 
 # ext=$(echo file_path##*.)
 # extension_list=("png" "jpg")
-# 
+#
 # flag=false
-# 
+#
 # for e in extension_list
 #    do
 #       if [ $e == ext ]; then
 #          $flag=true
 #       fi
 #    done
-# 
+#
 # if ! $flag; then
 #    echo "error!"
 #    exit $?
 # fi
 
-
-size=$(identify -format "%wx%h" $file_path)
-
-echo $size
 
 convert -size $size $file_path -fx "i % $gw == 0 || j % $gh == 0 ? 0 : p" grid_$file_path
 
--- src/main/scala/fontGLO/convAutoEnc.scala
+++ src/main/scala/fontGLO/convAutoEnc.scala
@@ -9,6 +9,8 @@ object convAE {
 
   import param._
 
+  val rand = new util.Random(0)
+
   def main(args: Array[String]) {
 
     param.setParamFromArgs(args)
@@ -28,8 +30,10 @@ object convAE {
     val test_d = data.drop(data_size)
 
     var Z = Array.ofDim[DenseVector[Double]](data_size)
-    Z = (Z zip train_d).map { case (z, td) =>
-      td
+    Z = Z.map { _ =>
+      DenseVector.fill(32*32) {
+        rand.nextGaussian / math.sqrt(data_size)
+      }
     }
 
 
@@ -39,7 +43,7 @@ object convAE {
     // training
     for (e <- 0 until epoch) {
 
-      val train = g.batch_train(Z, train_d, batch, err.calc_L2, grad.calc_L2_grad)
+      val train = g.batch_train(train_d, train_d, batch, err.calc_L2, grad.calc_L2_grad)
       val test = g.test(test_d, test_d, err.calc_L2)
       println(s"$e, E: ${train._1}, tE: ${test._1}")
 
@@ -50,14 +54,23 @@ object convAE {
         val filename = s"convAE_ds${data_size}_epoch${e}of${epoch}_batch$batch.txt"
 
         var ys = List[DenseVector[Int]]()
-        for (z <- Z) {
+        for (z <- train_d) {
           val y = g.predict(z).map(o => (o * 256).toInt)
           ys = y :: ys
           g.reset()
         }
 
+        // generate from Z space
+        var zs = List[DenseVector[Int]]()
+        for (z <- Z) {
+          val zy = g.predict(z+train_d.head).map(o => (o * 256).toInt)
+          zs = zy :: zs
+          g.reset()
+        }
+
         utils.write(s"${res_path}/${filename}", ys.reverse)
         utils.write(s"${res_path}/test_${filename}", test._2.map(i => convert(i * 256d, Int)))
+        utils.write(s"${res_path}/zspace_${filename}", zs.reverse)
 
         // save weights
         for (i <- g.layers.indices) {
@@ -65,9 +78,10 @@ object convAE {
           // MEMO:
           //   (g.layers(i).getClass()).toString ==> 'class pll.hogehoge'
           //   (g.layers(i).getClass()).toString.split(" ").last.drop(4) ==> 'hogehoge'
-
           g.layers(i).save(s"${weights_path}/${LAYER}_${i}_${filename}")
         }
+
+
       }
 
       // output Error
