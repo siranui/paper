14140cd5bd285d697967eeaa3aeefecb755d0abb
siranui <siranui87@gmail.com>
2017-11-10 01:54:51 +0900

    change

--- src/main/scala/pll/CastImplicits.scala
+++ src/main/scala/pll/CastImplicits.scala
@@ -2,10 +2,18 @@ package pll
 
 
 object CastImplicits {
-  import scala.language.implicitConversions._
   import breeze.linalg._
 
   implicit def Double2Float(n: Double) = n.toFloat
   implicit def Double2Float(n: DenseVector[Double]) = convert(n, Float)
   implicit def Double2Float(n: DenseMatrix[Double]) = convert(n, Float)
 }
+
+object Types {
+  import breeze.linalg._
+
+  type T = Float
+  type DV = DenseVector[T]
+  type DM = DenseMatrix[T]
+}
+
