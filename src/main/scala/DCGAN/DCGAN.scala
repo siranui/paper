86393f7ba9e9448aa0db6b549966ced9f87c4833
siranui <siranui87@gmail.com>
2017-11-21 00:20:41 +0900

    fix

--- src/main/scala/DCGAN/DCGAN.scala
+++ src/main/scala/DCGAN/DCGAN.scala
@@ -10,14 +10,16 @@ case class DCGAN(G: Generator, D: Discriminator) {
 
 object atMNIST {
 
-  val train_d = utils.read("/home/share/number/train-d.txt")
-  val test_d = utils.read("/home/share/number/test-d.txt")
-
   val D = Discriminator() // Loss-func is 'cross entropy'
   val G = Generator() // Loss-func is 'cross entropy'
   val dcgan = DCGAN(G, D)
 
-  def train(BATCH_SIZE: Int, NUM_EPOCH: Int, GENERATED_IMAGE_PATH: String) = {
+  def train(BATCH_SIZE: Int, NUM_EPOCH: Int)(TRAIN_DATA: String)(TEST_DATA: String)(GENERATED_IMAGE_PATH: String) = {
+
+    println(s"load data from:\n\ttrain data:\t$TRAIN_DATA\n\ttest data:\t$TEST_DATA")
+    val train_d = utils.read(TRAIN_DATA)
+    val test_d = utils.read(TEST_DATA)
+
     println("train start")
 
     val num_batches = (train_d.size / BATCH_SIZE).toInt
@@ -37,14 +39,10 @@ object atMNIST {
           save_gen_imgs(generated_image, s"${GENERATED_IMAGE_PATH}/${epoch}_${index}.txt")
         }
 
-
-
         // Update Discriminator
         val X: Array[DenseVector[Double]] = image_batch ++ generated_image
         val y: Array[DenseVector[Double]] = Array.fill(BATCH_SIZE){ DenseVector.ones[Double](1) } ++ Array.fill(BATCH_SIZE){ DenseVector.zeros[Double](1) }
         val (d_loss, d_ys_list) = D.model.batch_train(X, y, 2 * BATCH_SIZE, err.calc_cross_entropy_loss, grad.calc_cross_entropy_grad)
-
-
 
         // Update Generator
         val noise2 = Array.fill(BATCH_SIZE){ DenseVector.fill(100){ util.Random.nextDouble } }
@@ -73,6 +71,8 @@ object atMNIST {
 
   def main(args: Array[String]) {
 
+    var TRAIN_DATA = "/home/share/number/train-d.txt"
+    var TEST_DATA = "/home/share/number/test-d.txt"
     var BATCH_SIZE = 32
     var NUM_EPOCH = 20
     var GENERATED_IMAGE_PATH = "src/main/scala/DCGAN/img"
@@ -82,15 +82,17 @@ object atMNIST {
       val opt_value = args.grouped(2)
       opt_value.foreach{ ov =>
         ov(0) match {
-          case "--batch" => BATCH_SIZE = ov(1).toInt
-          case "--epoch" => NUM_EPOCH = ov(1).toInt
-          case "--save-path" => GENERATED_IMAGE_PATH = ov(1)
-          case _ => println(s"option[${ov(0)}] is not exist.")
+          case "--train-path" => TRAIN_DATA = ov(1)
+          case "--test-path"  => TEST_DATA = ov(1)
+          case "--batch"      => BATCH_SIZE = ov(1).toInt
+          case "--epoch"      => NUM_EPOCH = ov(1).toInt
+          case "--save-path"  => GENERATED_IMAGE_PATH = ov(1)
+          case _              => println(s"option[${ov(0)}] is not exist.")
         }
       }
     }
 
-    train(BATCH_SIZE, NUM_EPOCH, GENERATED_IMAGE_PATH)
+    train(BATCH_SIZE, NUM_EPOCH)(TRAIN_DATA)(TEST_DATA)(GENERATED_IMAGE_PATH)
 
   }
 
